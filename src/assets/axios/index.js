8a5efca53d6deaea6b0696f5354f30d54351e107
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2023-05-06 20:39:06 +0900

    비디오 리스트 퍼블리싱

--- src/assets/axios/index.js
+++ src/assets/axios/index.js
@@ -1,14 +1,21 @@
 import axios from "axios";
 //process.env.REACT_APP_SERVICE_VERSION
+
+function makeUrlParam(param) {
+  let urlParam = "?";
+  Object.keys(param).forEach((el) => {
+    urlParam += `${el}=${param[el]}&`;
+  });
+  return urlParam;
+}
 const instance = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
 });
 
+//
+
 export const api = {
   getList: (param) => {
-    return instance.get(
-      "/videos/?part=player&chart=mostPopular&key=AIzaSyACXoQ22yt8rGf9jcPqiLktxpcwPoMMyME",
-      param
-    );
+    return instance.get(`/videos${makeUrlParam(param)}`);
   },
 };
--- src/views/VideoList.jsx
+++ src/views/VideoList.jsx
@@ -1,20 +1,65 @@
 import "../App.scss";
-import { api } from "../assets/axios";
 import { useEffect, useState } from "react";
+//import { api } from "../assets/axios";
+const dayjs = require("dayjs");
 
+// function loadVideo(param) {
+//   return api.getList(param);
+// }
+
+function calculateDate(date) {
+  if (dayjs(date).diff(dayjs(), "year") < 0) {
+    return `${dayjs(date).diff(dayjs(), "year") * -1}년 전`;
+  } else if (dayjs(date).diff(dayjs(), "month") < 0) {
+    return `${dayjs(date).diff(dayjs(), "month") * -1}달 전`;
+  } else if (dayjs(date).diff(dayjs(), "day") < 0) {
+    return `${dayjs(date).diff(dayjs(), "day") * -1}일 전`;
+  } else if (dayjs(date).diff(dayjs(), "hour") < 0) {
+    return `${dayjs(date).diff(dayjs(), "hour") * -1}시간 전`;
+  } else if (dayjs(date).diff(dayjs(), "minute") < 0) {
+    return `${dayjs(date).diff(dayjs(), "minute") * -1}분 전`;
+  } else if (dayjs(date).diff(dayjs(), "second") < 0) {
+    return `${dayjs(date).diff(dayjs(), "second") * -1}초 전`;
+  }
+}
 function VideoList() {
   const [videoList, setVideoList] = useState([]);
 
   useEffect(() => {
-    api.getList().then((res) => {
-      console.log(res);
-      setVideoList(res);
-      console.log(videoList);
-    });
+    //   const param = {
+    //     part: "snippet",
+    //     chart: "mostPopular",
+    //     key: "AIzaSyACXoQ22yt8rGf9jcPqiLktxpcwPoMMyME",
+    //     maxResults: 20,
+    //   };
+    //   loadVideo(param).then(({ data }) => {
+    //     setVideoList(data.items);
+    //   });
+
+    fetch(`data/video.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        setVideoList(data);
+      });
   }, []);
+
   return (
-    <div className="Video-list">
-      <div></div>
+    <div className="AppBody">
+      <div className="VideoList inline-grid grid-cols-3 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 gap-4">
+        {videoList.map((el) => (
+          <div className="VideoItem row-span-2" key={el.id}>
+            <img
+              src={el.snippet.thumbnails.medium.url}
+              alt={el.snippet.title}
+            />
+            <div className="VideoTitle">
+              <h4>{el.snippet.title}</h4>
+              <h6>{el.snippet.channelTitle}</h6>
+              <h6>{calculateDate(el.snippet.publishedAt)}</h6>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
